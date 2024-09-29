73b7f8777788fe01e628bfab7ef83f6358fcb93f
Chris Dunphy <chris@megaparsec.ca>
2024-09-29 07:15:32 -0600

    Nixify

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "This is a program representing rational numbers in Python";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          # The packages we need for this project
+          buildInputs = with pkgs; [ python312 poetry pyright ruff ];
+        };
+      });
+}
